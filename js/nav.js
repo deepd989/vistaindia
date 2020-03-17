fb660f6a9afef0f06a6434772cd9c3df0334c902
deep <deepdama10>
2020-03-17 22:51:18 +0530

    First page 100% done

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,6 @@
+
+
+
 function hamburger() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -7,3 +10,31 @@ function hamburger() {
             x.style.display = "block";
         }
     }
+
+function make_active()
+{
+    let y=document.getElementById("rel");
+    y.classList.add("active_link_over");
+    console.log("done");
+}
+
+function make_deactive()
+{
+    let y=document.getElementById("rel");
+    y.classList.remove("active_link_over");
+    console.log("done");
+}
+
+function make_active2()
+{
+    let y=document.getElementById("rec");
+    y.classList.add("active_link_over2");
+    console.log("done");
+}
+
+function make_deactive2()
+{
+    let y=document.getElementById("rec");
+    y.classList.remove("active_link_over2");
+    console.log("done");
+}

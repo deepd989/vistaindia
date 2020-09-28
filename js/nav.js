04b0ceeb03688f47342e105ebcc68dcb2241ef3f
Deep Dama <you@domain.com>
2020-09-29 00:01:29 +0530

    homepage done

--- js/nav.js
+++ js/nav.js
@@ -15,26 +15,27 @@ function make_active()
 {
     let y=document.getElementById("rel");
     y.classList.add("active_link_over");
-    console.log("done");
+    // console.log("done");
 }
 
 function make_deactive()
 {
     let y=document.getElementById("rel");
     y.classList.remove("active_link_over");
-    console.log("done");
+    // console.log("done");
 }
 
 function make_active2()
 {
     let y=document.getElementById("rec");
     y.classList.add("active_link_over2");
-    console.log("done");
+    // console.log("done");
 }
 
 function make_deactive2()
 {
     let y=document.getElementById("rec");
     y.classList.remove("active_link_over2");
-    console.log("done");
+    // console.log("done");
 }
+

ecc0c0d5ebabcd4fd21e5e559fab3dfc1f9e314f
Deep Dama <you@domain.com>
2020-10-01 10:53:45 +0530

    fixed small bugs

--- js/caroursel.js
+++ js/caroursel.js
@@ -1,6 +1,6 @@
 
 $('.post-wrapper').slick({
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 2000,
@@ -10,21 +10,21 @@ $('.post-wrapper').slick({
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             infinite: true,
             dots: true
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 750,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2
           }
         },
         {
-          breakpoint: 480,
+          breakpoint: 500,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
